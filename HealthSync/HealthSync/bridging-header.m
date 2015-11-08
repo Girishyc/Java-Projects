29089dec7ee2ee8697534faa1a458592a3dddc73
Girish <girish02c@gmail.com>
2015-11-07 19:43:05 -0800

    Adding HealthSync
    With HMAC signature algorithm to generate signature…

---
+++ HealthSync/HealthSync/bridging-header.m
@@ -0,0 +1,10 @@
+//
+//  bridging-header.m
+//  HealthSync
+//
+//  Created by Girish Chaudhari on 11/1/15.
+//  Copyright © 2015 SwiftStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
